dbd7af72b0f76432678695a64e7870f616105916
Seannio <Sean_E_M@icloud.com>
2021-10-31 19:32:39 +0900

    npc diversification, stats, tileset changes

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def main() -> None:
     max_monsters_per_room = 2
 
     tileset = tcod.tileset.load_tilesheet(
-        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+        "Redjack17.png", 16, 16, tcod.tileset.CHARMAP_CP437
     )
 
     player = copy.deepcopy(entity_spawn.player)
